bc612d8cf32f9052d6f174af9c1eaf7f77245e35
Adam Fowler <adamfowler71@gmail.com>
2021-02-07 15:47:22 +0000

    Remove unnecessary route

--- Sources/todo-backend/Controllers/TodoController.swift
+++ Sources/todo-backend/Controllers/TodoController.swift
@@ -8,10 +8,9 @@ struct TodoController {
     func addRoutes(to group: HBRouterGroup) {
         group
             .get(use: list)
-            .get(":id", use: get)
             .post(use: create)
             .delete(use: deleteAll)
-            .patch(use: update)
+            .get(":id", use: get)
             .patch(":id", use: updateId)
             .delete(":id", use: deleteId)
     }
@@ -35,18 +34,6 @@ struct TodoController {
     func get(_ request: HBRequest) -> EventLoopFuture<Todo?> {
         guard let id = request.parameters.get("id", as: UUID.self) else { return request.failure(HBHTTPError(.badRequest)) }
         return Todo.find(id, on: request.db)
-    }
-
-    func update(_ request: HBRequest) -> EventLoopFuture<Todo> {
-        guard let newTodo = try? request.decode(as: Todo.self) else { return request.failure(HBHTTPError(.badRequest)) }
-        return Todo.query(on: request.db)
-            .filter(\.$title == "Test")
-            .first()
-            .unwrap(orError: HBHTTPError(.notFound))
-            .flatMap { todo -> EventLoopFuture<Todo> in
-                todo.update(from: newTodo)
-                return todo.update(on: request.db).map { todo }
-            }
     }
 
     func updateId(_ request: HBRequest) -> EventLoopFuture<Todo> {
