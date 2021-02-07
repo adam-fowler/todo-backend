2a66cf42c24b97224bd0b21c06c399842faef912
Adam Fowler <adamfowler71@gmail.com>
2021-02-07 15:39:11 +0000

    Initial commit

---
+++ Sources/todo-backend/app.swift
@@ -0,0 +1,40 @@
+import Foundation
+import FluentSQLiteDriver
+import Hummingbird
+import HummingbirdFluent
+import HummingbirdFoundation
+
+func runApp(_ arguments: HummingbirdArguments) throws {
+    let app = HBApplication(configuration: .init(address: .hostname(arguments.hostname, port: arguments.port)))
+    // set encoder and decoder
+    app.encoder = JSONEncoder()
+    app.decoder = JSONDecoder()
+    // middleware
+    app.middleware.add(HBLogRequestsMiddleware(.debug))
+    app.middleware.add(HBCORSMiddleware(
+        allowOrigin: .originBased,
+        allowHeaders: ["Content-Type"],
+        allowMethods: [.GET, .OPTIONS, .POST, .DELETE, .PATCH]
+    ))
+
+    // add Fluent
+    app.addFluent()
+    // add sqlite database
+    app.fluent.databases.use(.sqlite(.file("db.sqlite")), as: .sqlite)
+    // add migrations
+    app.fluent.migrations.add(CreateTodo())
+    // migrate
+    if arguments.migrate {
+        try app.fluent.migrate().wait()
+    }
+
+
+    app.router.get("/") { _ in
+        return "Hello"
+    }
+    let todoController = TodoController()
+    todoController.addRoutes(to: app.router.group("todos"))
+
+    app.start()
+    app.wait()
+}
