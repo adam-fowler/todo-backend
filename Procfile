061800dc9c73ee2bea2a3fd49ba7bf832fa3f9f6
Adam Fowler <adamfowler71@gmail.com>
2021-02-07 18:03:01 +0000

    port from environment

--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-web: todo-backend --hostname=0.0.0.0 --port $PORT
+web: todo-backend --hostname=0.0.0.0
--- Sources/todo-backend/main.swift
+++ Sources/todo-backend/main.swift
@@ -1,11 +1,14 @@
 import ArgumentParser
+import Hummingbird
 
 struct HummingbirdArguments: ParsableCommand {
+    static var env = HBEnvironment()
+    
     @Option(name: .shortAndLong)
     var hostname: String = "127.0.0.1"
 
     @Option(name: .shortAndLong)
-    var port: Int = 8080
+    var port: Int = env.get("PORT", as: Int.self) ?? 8080
 
     func run() throws {
         try runApp(self)
